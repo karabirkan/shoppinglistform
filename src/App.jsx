629ccef58279780825709351116e9099253d10e3
karabirkan <115654236+tokyoparis@users.noreply.github.com>
2023-06-10 13:09:02 +0300

    Shopping List function created seperatly

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import "./App.css";
-import ShoppingListForm from "./ShoppingListForm";
+import ShoppingList from "./ShoppingList";
 
 function App() {
   return (
     <>
-      <ShoppingListForm />
+      <ShoppingList />
     </>
   );
 }
--- src/ShoppingListForm.jsx
+++ src/ShoppingListForm.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
-export default function ShoppingListForm() {
+export default function ShoppingListForm({ addItem }) {
   const [formData, setFormData] = useState({ product: "", quantity: 0 });
   const handleChange = (evt) => {
     setFormData((currData) => {
       return { ...currData, [evt.target.name]: evt.target.value };
     });
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addItem(formData);
+  };
   return (
-    <form action="">
+    <form action="" onSubmit={handleSubmit}>
       <h1>
         Product is : {formData.product} and quantity is : {formData.quantity}
       </h1>
