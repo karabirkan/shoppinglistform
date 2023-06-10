abcbd955451967bed9cf383bc6799f012804d8c0
karabirkan <115654236+tokyoparis@users.noreply.github.com>
2023-06-10 12:46:20 +0300

    Shopping List Form Component created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import ShoppingListForm from "./ShoppingListForm";
+
+function App() {
+  return (
+    <>
+      <ShoppingListForm />
+    </>
+  );
+}
+
+export default App;
---
+++ src/ShoppingListForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+export default function ShoppingListForm() {
+  const [formData, setFormData] = useState({ product: "", quantity: 0 });
+  const handleChange = (evt) => {
+    setFormData((currData) => {
+      return { ...currData, [evt.target.name]: evt.target.value };
+    });
+  };
+  return (
+    <form action="">
+      <h1>
+        Product is : {formData.product} and quantity is : {formData.quantity}
+      </h1>
+      <label htmlFor="product"></label>
+      <input
+        type="text"
+        placeholder="product name"
+        name="product"
+        id="product"
+        onChange={handleChange}
+        value={formData.product}
+      />
+      <label htmlFor="quantity"></label>
+      <input
+        type="number"
+        placeholder="1"
+        name="quantity"
+        id="quantity"
+        onChange={handleChange}
+        value={formData.quantity}
+      />
+      <button>Add Item</button>
+    </form>
+  );
+}
